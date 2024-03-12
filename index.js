c0b9a5b0ac2023c5a6b800c092b391bfaae40178
sathish <sathis.nsk@gmail.com>
2024-03-12 21:49:25 +0530

    test response

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const port = 3000;
 const server = http.createServer(function(req, res)
 {
 
+
+res.write("test response");
+
+res.end();
+
 });
 
 server.listen(port, (error) => {
     if (!error) {
         console.log(`listening on port ${port}`);
     } else {
-        console.log()
+        console.log();
     }
 
 });
