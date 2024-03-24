cca61515bf7ba77f97a9b2ca233f8bfaa6730197
sathish <sathis.nsk@gmail.com>
2024-03-24 12:30:32 +0530

    with log

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const PORT = 3000;
 const httpServer = http.createServer(function(req, res)
 {
 
+
+    console.log(req.url);
+    console.log(req.path);
+   // console.log(req);
+
+   const {method, url, headers} = req;
+
+      fs.appendFile('./server_log.txt', `\n log time ${Date.now()} \n ${headers} \n ${method} \n ${url}`, (err)=> {} );
+
         const options = {
             'Content-Type': 'text/html',
-            'Access-Control-Allow-Origin': 'https://candid-florentine-c99a0d.netlify.app/'
+            'Access-Control-Allow-Origin': '*'
         }
 //res.setHeader('Content-Type', 'text/html');
 //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
 //res.setHeader('Access-Control-Allow-Origin', 'https://candid-florentine-c99a0d.netlify.app/');
+                                                
+ 
 res.writeHead(200,options);
 fs.readFile('./index.html','utf-8', (err,data) => {
     res.write(data);
