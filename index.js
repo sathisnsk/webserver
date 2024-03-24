2d74364ddf44ce2a0e876d7d469af1e9ba81b4a3
sathish <sathis.nsk@gmail.com>
2024-03-24 08:35:19 +0530

    with CORS

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const PORT = 3000;
 const httpServer = http.createServer(function(req, res)
 {
 
-res.setHeader('Content-Type', 'text/html');
+        const options = {
+            'Content-Type': 'text/html',
+            'Access-Control-Allow-Origin': 'https://candid-florentine-c99a0d.netlify.app/'
+        }
+//res.setHeader('Content-Type', 'text/html');
 //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
-res.setHeader('Access-Control-Allow-Origin', 'https://candid-florentine-c99a0d.netlify.app/');
+//res.setHeader('Access-Control-Allow-Origin', 'https://candid-florentine-c99a0d.netlify.app/');
+res.writeHead(200,options);
 fs.readFile('./index.html','utf-8', (err,data) => {
     res.write(data);
 })
