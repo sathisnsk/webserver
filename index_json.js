f2a87594b08cb37cde3a8bf68b522ea932077235
sathish <sathis.nsk@gmail.com>
2024-04-07 20:04:47 +0530

    working one

--- index_json.js
+++ index_json.js
@@ -7,31 +7,28 @@ const httpServer = http.createServer(function(req, res)
 {
     console.log(req.url);
     console.log(req.path);
-    const {method, url, headers} = req;
-
+   const {method, url, headers} = req;
       fs.appendFile('./server_log.txt', `\n log time ${Date.now()} \n ${headers} \n ${method} \n ${url}`, (err)=> {} );
-
         const options = {
-            'Content-Type': 'text/html',
-            'Access-Control-Allow-Origin': '*'
-        }
-
-       /*  const options = {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
-        } */
- 
+        }
+//res.setHeader('Content-Type', 'text/html');
+//res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+//res.setHeader('Access-Control-Allow-Origin', 'https://candid-florentine-c99a0d.netlify.app/');
+                                                
 res.writeHead(200,options);
 
-fs.readFile('./index.html','utf-8', (err,data) => {
-    console.log(data);
-    res.write(data);
-    res.end();
-});
+//fs.readFile('./index.html','utf-8', (err,data) => {
+  //  res.write(data);
+//});
 
-/* const testJson = {"name":"kumar", "age":25}
-res.write(JSON.stringify(testJson)); */
-//res.write('<h1>server Response</h1>');
+const testJson = {"name":"kumar", "age":25};
+
+res.write(JSON.stringify(testJson));
+
+res.end();
+
 });
 
 httpServer.listen(PORT, (error) => {
